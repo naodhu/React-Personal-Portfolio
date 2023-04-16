945f77a0bea6ec60e50321a2be7bb3722549210e
Naod Hunde <chris_naod@yahoo.com>
2023-04-17 03:03:42 +1000

    final commit

---
+++ src/data/data.js
@@ -0,0 +1,33 @@
+import TravelGuide from "../assets/projects/project1.png";
+import ChatSupport from "../assets/projects/project2.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "React JS Application",
+    image: TravelGuide,
+    github: "https://github.com/naodhu/australian-travel-guide",
+    live: "https://naodhu.github.io/australian-travel-guide/",
+  },
+  {
+    id: 2,
+    name: "React JS Application",
+    image: ChatSupport,
+    github: "https://github.com/naodhu/chat-support1",
+    live: "https://chat-support-999.herokuapp.com/",
+  },
+  //   {
+  //     id: 3,
+  //     name: "React JS Application",
+  //     image: WorkIm,
+  //     github: "",
+  //     live: "",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "React JS Application",
+  //     image: RealEst,
+  //     github: "",
+  //     live: "",
+  //   },
+];
